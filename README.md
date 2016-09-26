9dae0a5c7c2effd0c91405805a244fcaa80c63c3
crmaykish <crmaykish@gmail.com>
2016-09-26 00:00:33 -0400

    Project init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ultrasonic-sensor-board-10x
+# Ultrasonic Sensor Board 10x
+
 Dedicated sensor board for reading 10 ultrasonic ping sensors and passing the measurements over serial
+
+![Sensor board](board.jpg)
---
+++ ultrasonic_board/main.c
@@ -0,0 +1,18 @@
+/*
+ * ultrasonic_board.c
+ *
+ * Created: 9/25/2016 11:54:17 PM
+ * Author : Colin Maykish
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
