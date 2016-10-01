a74abb2abfda8a24f2409f1f6fa1527754b3f5ac
crmaykish <crmaykish@gmail.com>
2016-10-01 18:22:16 -0400

    JSON sensor data output over serial

--- ultrasonic_board/main.c
+++ ultrasonic_board/main.c
@@ -1,21 +1,31 @@
 /*
- * Ultrasonic Sensor Board 10x
- *
- * Created: 9/25/2016 11:54:17 PM
- * Author : Colin Maykish
- * https://github.com/crmaykish/ultrasonic-sensor-board-10x
- */ 
+* Ultrasonic Sensor Board 10x
+*
+* Created: 9/25/2016 11:54:17 PM
+* Author : Colin Maykish
+* https://github.com/crmaykish/ultrasonic-sensor-board-10x
+*/
 
 #include <avr/io.h>
 #include "pins.h"
 #include "bitmacros.h"
 #include "serial.h"
 #include "ultrasonic.h"
-#include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>
 
-Ultrasonic s8 = {&S_PORT8, &S_PIN8, &S_DDR8, S_TRIG8, S_ECHO8};
+Ultrasonic sensors[10] = {
+	{&S_PORT0, &S_PIN0, &S_DDR0, S_TRIG0, S_ECHO0},
+	{&S_PORT1, &S_PIN1, &S_DDR1, S_TRIG1, S_ECHO1},
+	{&S_PORT2, &S_PIN2, &S_DDR2, S_TRIG2, S_ECHO2},
+	{&S_PORT3, &S_PIN3, &S_DDR3, S_TRIG3, S_ECHO3},
+	{&S_PORT4, &S_PIN4, &S_DDR4, S_TRIG4, S_ECHO4},
+	{&S_PORT5, &S_PIN5, &S_DDR5, S_TRIG5, S_ECHO5},
+	{&S_PORT6, &S_PIN6, &S_DDR6, S_TRIG6, S_ECHO6},
+	{&S_PORT7, &S_PIN7, &S_DDR7, S_TRIG7, S_ECHO7},
+	{&S_PORT8, &S_PIN8, &S_DDR8, S_TRIG8, S_ECHO8},
+	{&S_PORT9, &S_PIN9, &S_DDR9, S_TRIG9, S_ECHO9}
+};
 
 void setup() {
 	serialInit();
@@ -26,14 +36,20 @@ void setup() {
 	// turn on timer at 1/8 scaling
 	TCCR1B = bitValue(CS11);
 	
-	ultrasonic_init(&s8);
+	for (int i = 0; i < 10; i++) {
+		ultrasonic_init(&sensors[i]);
+	}
 }
 
+// Read all sensors and output JSON over serial
 void loop() {
-	unsigned int distance = ultrasonic_read(&s8);
-	printf("Distance: %d\n", distance);
-	
-	_delay_ms(60);
+	printf("{ \"sensors\" : [");
+	for (int i = 0; i < 10; i++) {
+		unsigned int distance = ultrasonic_read(&sensors[i]);
+		printf("{ \"pos\" : %d, \"val\" : %d }", i, distance);
+		if (i != 9) printf(", ");
+	}
+	printf("] }\n");
 }
 
 int main(void) {
--- ultrasonic_board/ultrasonic.c
+++ ultrasonic_board/ultrasonic.c
@@ -15,6 +15,7 @@ unsigned int timer_val() {
 	return (count);
 }
 
+// Send a trigger pulse to start the sensor
 void _ultrasonic_trigger(Ultrasonic *u) {
 	clearBit(*(u->port), u->trigger);
 	_delay_us(1);
@@ -23,24 +24,27 @@ void _ultrasonic_trigger(Ultrasonic *u) {
 	clearBit(*(u->port), u->trigger);
 }
 
+// Set trigger pin to output and echo to input
 void ultrasonic_init(Ultrasonic *u) {
 	setBit(*(u->ddr), u->trigger);
 	clearBit(*(u->ddr), u->echo);
 }
 
+// Read sensor distance
 unsigned int ultrasonic_read(Ultrasonic *u) {
+	// Trigger the sensor and start timing
 	_ultrasonic_trigger(u);
-	
 	reset_timer();
 	
+	// Wait until ECHO pin goes HIGH
 	while (!queryBit(*(u->pin), u->echo)) {
 		if (timer_val() >= TRIGGER_TIMEOUT) {
 			return TRIGGER_ERROR;
 		}
 	}
-	
+
+	// Reset timer and wait until ECHO pin goes back to LOW
 	reset_timer();
-	
 	while (queryBit(*(u->pin), u->echo)) {
 		if (timer_val() >= ECHO_TIMEOUT) {
 			return ECHO_ERROR;
